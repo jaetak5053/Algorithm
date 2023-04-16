71387d0411b31da0ec92b878dd70d20493a10b25
unknown <nohtak1213@gmail.com>
2023-04-16 17:27:20 +0900

    20230416

---
+++ 백준/1448_삼각형만들기.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def fun(arr):
+    i = 0
+    while i+2 < len(arr):
+        if arr[i] < arr[i+1] + arr[i+2]:
+            return arr[i]+arr[i+1] + arr[i+2]
+        i += 1
+    return -1
+
+
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+arr.sort(reverse=True)
+
+print(fun(arr))
+
+# int(input()) 보다 sys.stdin.readline()이 더 속도가 빠름
